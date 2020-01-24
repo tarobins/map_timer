b27f85b6ad35c9020258614dab2c15234f77cf92
robinsontom <robinsontom@google.com>
2020-01-23 20:40:00 -0800

    Create project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:map_timer/map_timer.dart' as map_timer;
+
+main(List<String> arguments) {
+  print('Hello world: ${map_timer.calculate()}!');
+}
---
+++ test/map_timer_test.dart
@@ -0,0 +1,8 @@
+import 'package:map_timer/map_timer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
